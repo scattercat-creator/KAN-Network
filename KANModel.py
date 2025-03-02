c3669c7d927f97ac3b9fffc4289b0e47419be2ef
Shubham Waldiya <69055419+HiThere-12345@users.noreply.github.com>
2025-03-02 04:51:29 +0000

    initial KAN building

---
+++ KANModel.py
@@ -0,0 +1,55 @@
+import torch as torch
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+from kan import KAN, create_dataset
+#literally copied the code below from the documentation.  Need to figure out what does what
+
+# Set the default data type to double
+torch.set_default_dtype(torch.float64)
+#set device
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+'''
+initialize model
+- refer to MultKAN.py for more information
+    width: number of neurons in each layer, in order from input to output
+    k: order of the spline
+    seed: random seed
+    grid: grid intervals/grid points (affects the accuracy of the splines/learnable functions)
+'''
+model = KAN(width=[2,5,1], grid=5, k=3, seed=0, device=device)
+
+#to-do: modify f into a function that returns the values from out dataset
+f = lambda x: torch.exp(torch.sin(torch.pi*x[:,[0]]) + x[:,[1]]**2) # function to approximate
+# info on create_dataset in utils.py
+dataset = create_dataset(f, n_var=2, device=device) # creates a dataset from a function f. f itself is a function that takes in x
+#will need to replace create_dataset with the following:
+ourdata = {}
+'''
+ourdata['train_input'] = torch.tensor() #contains the training data, each data is the binary representation
+of an image as per the MNIST dataset
+ourdata['test_input'] = torch.tensor() #contains the testing data, same format as train_input
+ourdata['train_label'] = torch.tensor() #contains the labels for the training data
+ourdata['test_label'] = torch.tensor() #contains the labels for the testing data
+'''
+
+model(dataset['train_input']) #forward pass of the model
+model.plot() #plots the model
+
+#code to train the model
+'''
+Training the model off the dataset
+- opt: optimization method (LBFGS)
+- steps: training steps
+- lamb: penalty parameter
+other parameters: lr = learning rate = 1, loss_fn = loss function = None
+
+#fits the model to the dataset
+model.fit(dataset, opt="LBFGS", steps=50, lamb=0.001) #values from the basic example in the documentation
+model.plot() #plots the model
+
+model = model.prune()
+model.plot()
+'''
