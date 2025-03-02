9e4af145198d17edcbf33204e175266abdea8636
Shubham Waldiya <69055419+HiThere-12345@users.noreply.github.com>
2025-03-02 06:04:54 +0000

    nothing

---
+++ DataRead.py
@@ -0,0 +1,9 @@
+import numpy as np
+import torch
+import matplotlib.pyplot as plt
+
+train_images_path = "/Dataset/train-images-idx3-ubyte/train-images-idx3-ubyte"
+train_labels_path = "/Dataset/train-labels-idx1-ubyte/train-labels-idx1-ubyte"
+test_images_path = "/Dataset/t10k-images-idx3-ubyte/t10k-images-idx3-ubyte"
+test_labels_path = "/Dataset/t10k-labels-idx1-ubyte/t10k-labels-idx1-ubyte"
+
