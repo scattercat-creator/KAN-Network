979bf74f48061380a56c9c1f78f5fd588eff8894
Shubham Waldiya <69055419+HiThere-12345@users.noreply.github.com>
2025-03-08 21:37:39 +0000

    A few changes

--- DataRead.py
+++ DataRead.py
@@ -15,14 +15,13 @@ def read_idx(filename):
 class OurData:
     def __init__(self):
         self.ourdataset = {}
-        print("Reading data")
         train_images = read_idx('/workspaces/KAN-Network/Dataset/train-images-idx3-ubyte/train-images-idx3-ubyte') #contains the training data, each data is the binary representation of an image as per the MNIST dataset
         self.ourdataset['train_input'] = (train_images).view(-1, 28*28) #reshapes the data into a 2D array, each row is an image
         test_images = read_idx('/workspaces/KAN-Network/Dataset/t10k-images-idx3-ubyte/t10k-images-idx3-ubyte') #contains the testing data, same format as train_input
         self.ourdataset['test_input'] = (test_images).view(-1, 28*28) #reshapes the data into a 2D array, each row is an image
-        self.ourdataset['train_label'] = read_idx('/workspaces/KAN-Network/Dataset/train-labels-idx1-ubyte/train-labels-idx1-ubyte') #contains the labels for the training data
-        self.ourdataset['test_label'] = read_idx('/workspaces/KAN-Network/Dataset/t10k-labels-idx1-ubyte/t10k-labels-idx1-ubyte') #contains the labels for the testing data
-        print("These are our training inputs and labels")
+        self.ourdataset['train_label'] = read_idx('/workspaces/KAN-Network/Dataset/train-labels-idx1-ubyte/train-labels-idx1-ubyte').unsqueeze(1) #contains the labels for the training data
+        self.ourdataset['test_label'] = read_idx('/workspaces/KAN-Network/Dataset/t10k-labels-idx1-ubyte/t10k-labels-idx1-ubyte').unsqueeze(1) #contains the labels for the testing data
+        #print("These are our training inputs and labels")
     def __getitem__(self):
         return self.ourdataset
     def __getitem__(self, key):
--- KANModel.py
+++ KANModel.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from DataRead import OurData
+import traceback
 
 from kan import KAN, create_dataset
 #literally copied the code below from the documentation.  Need to figure out what does what
@@ -21,8 +22,8 @@ initialize model
     seed: random seed
     grid: grid intervals/grid points (affects the accuracy of the splines/learnable functions)
 '''
-model = KAN(width=[2, 5, 1], grid=5, k=3, seed=0, device=device)
-model2 = KAN(width=[2, 5, 1], grid=5, k=3, seed=0, device=device)
+model = KAN(width=[784, 5, 1], grid=5, k=3, seed=0, device=device)
+#model2 = KAN(width=[2, 5, 1], grid=5, k=3, seed=0, device=device)
 
 #to-do: modify f into a function that returns the values from out dataset
 
@@ -30,19 +31,21 @@ model2 = KAN(width=[2, 5, 1], grid=5, k=3, seed=0, device=device)
 data = OurData()
 ourdata = {}
 for key in data.ourdataset:
-    ourdata[key] = data.ourdataset[key]
+    ourdata[key] = (data.ourdataset[key])[:10] #only get the first 10 data points for now
 
-model(ourdata['train_input']) #forward pass of the model
-model.plot() #plots the model
-print("done")
-'''
-f = lambda x: torch.exp(torch.sin(torch.pi*x[:,[0]]) + x[:,[1]]**2)
+ #check the shape of our dataset against the create_dataset function which we know works
+f = lambda x: x[:, [0]] + x[:, [1]] # This function takes a 2D tensor and returns the sum of the first and second columns
 dataset = create_dataset(f, n_var=2, device=device)
-model2(dataset['train_input'])
-model2.plot()
-print(dataset['train_input'], ourdata['train_input'])
 '''
-
+print(dataset['train_input'].shape, ourdata['train_input'].shape)
+print(dataset['train_label'].shape, ourdata['train_label'].shape)
+    #checking to see if the data points are only 0s(it looks like it from the outputs but they are not)
+print(ourdata['train_input'][1])
+print(ourdata['train_input'][2])
+'''
+ourdata['train_input'] = ourdata['train_input'] + 1 #add 1 to the data points to make them non-zero. Didnt fix the coef error
+model(ourdata['train_input']) #forward pass of the model
+#model.plot() #plots the model, avoid doing this since it will plot functions for all the neurons(and we have a lot since we are dealing with images)
 #code to train the model
 '''
 Training the model off the dataset
@@ -52,11 +55,15 @@ Training the model off the dataset
 other parameters: lr = learning rate = 1, loss_fn = loss function = None
 '''
 #fits the model to the dataset
-'''
-model.fit(ourdata, opt="LBFGS", steps=50, lamb=0.001) #values from the basic example in the documentation
-model.plot() #plots the model
+try:
+    #currently doesn't work since all the coefficients are uninitialized???
+    model.fit(ourdata, opt="LBFGS", steps=50, lamb=0.001,) #values from the basic example in the documentation
+#model.plot() 
+except Exception as e:
+    print(e)
+    traceback.print_exc()
 print("done")
-
+'''
 model = model.prune()
 model.plot()
 '''
