e93a1d7ca38f0c0bc619cf978423352ced0de34c
Shubham Waldiya <69055419+HiThere-12345@users.noreply.github.com>
2025-03-14 19:19:05 +0000

    some changes

--- KANModel.py
+++ KANModel.py
@@ -26,10 +26,10 @@ ourdata = {}
 ourdata2 = {}
 ourdata3 = {}
 for key in data.ourdataset:
-    #total train datapoints=60000, test datapoints=10000. approx time:
-    ourdata[key] = data.ourdataset[key][:100] #only get the first 100 data points for now
-    ourdata2[key] = getsliced(data.ourdataset[key], 28)[:1000] #each element is a sum of a row of the 2d array
-    ourdata3[key] = getsliced(data.ourdataset[key], 3)[:2000] #each element is a sum of a third of the entire 2d array
+    #total train datapoints=60000, test datapoints=10000. approx time:1min/100 points model1, 30s/1000 points model2, 1min/2500 points model3
+    ourdata[key] = data.ourdataset[key][:200] #only get the first 200 data points for now
+    ourdata2[key] = getsliced(data.ourdataset[key], 28)[:4000] #each element is a sum of a row of the 2d array
+    ourdata3[key] = getsliced(data.ourdataset[key], 3)[:5000] #each element is a sum of a third of the entire 2d array
 
 '''
 initialize model
@@ -60,12 +60,14 @@ model3(ourdata3['train_input'])
 #model.plot() #plots the model, avoid doing this since it will plot functions for all the neurons(and we have a lot since we are dealing with images)
 
 ''' code to now fit the model to the dataset
+
 Training the model off the dataset
 - opt: optimization method (LBFGS)
 - steps: training steps
 - lamb: penalty parameter
 other parameters: lr = learning rate = 1, loss_fn = loss function = None
 '''
+#steps = intervals to divide the dataset and update model, epochs = how many times the entire dataset is passed through the model
 modelresults = []
 modelresults.append(model.fit(ourdata, opt="LBFGS", steps=50, lamb=0.001)) #values from the basic example in the documentation
 modelresults.append(model2.fit(ourdata2, opt="LBFGS", steps=50, lamb=0.001))
@@ -79,7 +81,38 @@ evaluation_results = [eval1, eval2, eval3]
 test_losses = [[result['test_loss'] for result in evaluation_results]]
 
 testingdata = data.getitems(9950, 10000) 
-results = model.forward(testingdata[0])
-results2 = model2.forward(getsliced(testingdata[0], 28))
-results3 = model3.forward(getsliced(testingdata[0], 3))
+predictions = model.forward(testingdata[0])
+predictions2 = model2.forward(getsliced(testingdata[0], 28))
+predictions3 = model3.forward(getsliced(testingdata[0], 3))
+allpredictions = [predictions, predictions2, predictions3]
+
+for i in range(3):
+    plt.plot(range(len(allpredictions[i])), allpredictions[i].detach().numpy(), label=f'Model{i+1} Predictions')
+plt.plot(range(len(testingdata[1])), testingdata[1].detach().numpy(), label='Actual')
+plt.xlabel('Input Number')
+plt.ylabel('Output')
+plt.title('Model Predictions vs Actual')
+plt.legend()
+plt.show()
+
+error = [0, 0, 0]
+correct = [0, 0, 0]
+for i in range(len(testingdata[1])):
+    print(i, end="\t")
+    for j in range(3):
+        print(j+1, allpredictions[j][i].item(), end="\t")
+        difference = abs(allpredictions[j][i].item()-i)
+        error[j] += 1 if difference > 1 else difference
+    print()
+for j in range(3):
+    error[j] /= len(testingdata[1])
+    correct[j] = 100 - 100*error[j]
+
+plt.scatter([i for i in range(1, 4)], error, label='Average Error') 
+plt.scatter([i for i in range(1, 4)], correct, label='Accuracy') 
+plt.xlabel('Model Number')
+plt.title('Scatter Plot of Models')
+plt.legend()
+print(error)
+print(correct)
 
